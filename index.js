e0454d3cf8e6725cc5b3575f70e6cfe945e490f6
Linus Ljung <linus.ljung@na-kd.com>
2017-02-07 08:04:05 +0100

    Updated matches and added elo  rating

--- index.js
+++ index.js
@@ -1,36 +1,48 @@
 const Glicko2 = require('glicko2').Glicko2;
-const ranking = new Glicko2({
-  tau: .5,
-  rating: 1500,
-  rd: 350,
-  vol: .06
-});
+const Elo = require('elo-calculator');
+const calculators = {
+  glicko: new Glicko2({
+    tau: .5,
+    rating: 1500,
+    rd: 350,
+    vol: .06
+  }),
+  elo: new Elo()
+};
 
 const players = ['Joel', 'Anders', 'Jonas', 'David', 'Linus', 'Johan', 'Pär', 'Zebbe', 'Daniel']
   .reduce(function(players, player) {
     return Object.assign({}, players, {
-      [player]: ranking.makePlayer()
+      [player]: {
+        glicko: calculators.glicko.makePlayer(),
+        elo: calculators.elo.createPlayer()
+      }
     });
   }, {});
 
-function createMatch(players, challanger, defender, result) {
-  return [players[defender], players[challanger], result];
+function createMatch(players, challanger, defender, result, calculator) {
+  return [players[defender][calculator], players[challanger][calculator], result];
 }
 
-const matches = [
-  createMatch(players, 'Joel', 'Anders', 1),
-  createMatch(players, 'Jonas', 'David', 1),
-  createMatch(players, 'Linus', 'Jonas', 1),
-  createMatch(players, 'Anders', 'Johan', 1),
-  createMatch(players, 'Pär', 'Zebbe', 1),
-  createMatch(players, 'Daniel', 'Linus', 0)
-];
+function updateRatings() {
+  ['elo', 'glicko'].forEach(function(calculator) {
+    calculators[calculator].updateRatings([
+      createMatch(players, 'Joel', 'Anders', 1, calculator),
+      createMatch(players, 'Jonas', 'David', 1, calculator),
+      createMatch(players, 'Linus', 'Jonas', 1, calculator),
+      createMatch(players, 'Anders', 'Johan', 1, calculator),
+      createMatch(players, 'Pär', 'Zebbe', 1, calculator),
+      createMatch(players, 'Linus', 'Daniel', 1, calculator),
+      createMatch(players, 'Linus', 'Daniel', 1, calculator)
+    ])
+  });
+}
 
-ranking.updateRatings(matches);
+updateRatings();
 
 console.log(Object.keys(players).reduce(function(playerList, player) {
   return [
     ...playerList,
-    `${player}: ${players[player].getRating()}`
+    `${player}: Glicko:  ${Math.round(players[player].glicko.getRating())} Elo: ${Math.roundplayers[player].elo.rating}`
   ];
 }, []));
